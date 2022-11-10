ca8a4b3e5217869e5b2eb4d2eeb9a496bb033ab8
MTez <mtez1@example.com>
2022-11-10 12:25:31 +0100

    OK

--- ILFilePacifier/Program.cs
+++ ILFilePacifier/Program.cs
@@ -15,7 +15,7 @@ namespace ILFilePacifier
     Generates ROBOCOPY commands to move files from various sources to a common destination.
     i.e.
 
-    matches.csv
+    matchers.csv
 
     what to match;where to move
     -----------------------------------------
@@ -89,16 +89,36 @@ namespace ILFilePacifier
                 }
             }
 
-            var matchers = ILMatch.LoadFromCSV (@"matches.csv");
+            var matchers = ILMatch.LoadFromCSV (@"matchers.csv");
 
             foreach (var matcher in matchers) {
 
-                var foundsff = new List<String> ();
                 var foundsdd = new List<String> ();
+                var foundsff = new List<String> ();
 
                 foreach (var where in wheres) {
 
                     foreach (var what in matcher.What) {
+
+                        // find matching dirs
+                        try {
+                            var dd = Directory.GetDirectories (
+                            where,
+                            ILMatch.ToWildcard (what),
+                            System.IO.SearchOption.AllDirectories
+                            );
+
+                            foreach (var d in dd) {
+                                var di = new DirectoryInfo (d);
+
+                                if (di.GetFiles ().Length > 0)// && Operators.LikeString (d, ILMatch.ToWildcard(what), Microsoft.VisualBasic.CompareMethod.Text))
+                                    foundsdd.Add (d);
+                            }
+                        } catch (Exception x) {
+                            Console.Error.WriteLine ("Err {0}", x.Message);
+                        }
+
+                        // find matching files
                         try {
                             var ff = Directory.GetFiles (
                                 where,
@@ -116,34 +136,66 @@ namespace ILFilePacifier
                         } catch (Exception x) {
                             Console.Error.WriteLine ("Err {0}", x.Message);
                         }
-
-                        try {
-                            var dd = Directory.GetDirectories (
-                            where,
-                            ILMatch.ToWildcard (what),
-                            System.IO.SearchOption.AllDirectories
-                            );
-
-                            foreach (var d in dd) {
-                                var di = new DirectoryInfo (d);
-
-                                if (di.GetFiles ().Length > 0)// && Operators.LikeString (d, ILMatch.ToWildcard(what), Microsoft.VisualBasic.CompareMethod.Text))
-                                    foundsdd.Add (d);
-                            }
-                        } catch (Exception x) {
-                            Console.Error.WriteLine ("Err {0}", x.Message);
-                        }
                     }
 
                 }
 
-                int c = 0;
-                foreach (var found in foundsff) {
-                    var lines = new String[] {
-                        string.Format ("TITLE PROCESSING F {0} / {1} - \"{2}\"\r\n", ++c, foundsff.Count, found),
+                GenerateMoveCmds_Dir (matcher, foundsdd);
+                GenerateMoveCmds_File (matcher, foundsff);
+
+            }
+
+        }
+
+        private static void GenerateMoveCmds_Dir (ILMatch matcher, List<string> foundsdd)
+        {
+            int c = 0;
+            foreach (var found in foundsdd) {
+                var lines = new String[] {
+                        string.Format ("TITLE PROCESSING D {0} / {1} [ {2} ] \"{3}\"\r\n", ++c, foundsdd.Count, matcher.What.FirstOrDefault (), found),
 
                         string.Format ("IF EXIST   \"{0}\" PAUSE \r\n",
-                        Path.GetDirectoryName(matcher.Destination) + Path.DirectorySeparator + Path.GetFileName(found)
+                        Path.GetDirectoryName(matcher.Destination) + Path.DirectorySeparator + new DirectoryInfo(found).Name
+                        ),
+
+                        string.Format ("IF EXIST   \"{0}\" (\r\n" +
+                        "  ROBOCOPY \"{0}\" \"{1}\" /MOV\r\n" +
+                        "  RMDIR    \"{0}\"\r\n" +
+                        ")\r\n\r\n",
+
+                        found,
+                        Path.GetDirectoryName(matcher.Destination) + Path.DirectorySeparator + new DirectoryInfo(found).Name
+                        ),
+
+                    };
+
+                File.AppendAllLines (
+                    "Z_!ALL_1D.cmd",
+                    lines,
+                    Encoding.Default);
+
+                File.AppendAllLines (
+                    "Z_" + matcher.What.FirstOrDefault () + "_1D.cmd",
+                    lines,
+                    Encoding.Default);
+            }
+        }
+
+        private static void GenerateMoveCmds_File (ILMatch matcher, List<string> foundsff)
+        {
+            int c = 0;
+            foreach (var found in foundsff) {
+                var lines = new String[] {
+                        string.Format ("TITLE PROCESSING F {0} / {1} [ {2} ] \"{3}\"\r\n", ++c, foundsff.Count, matcher.What.FirstOrDefault (), found),
+
+                        string.Format ("IF EXIST   \"{0}\" (\r\n" +
+                        "  Certutil -hashfile \"{1}\"\r\n" +
+                        "  Certutil -hashfile \"{0}\"\r\n" +
+                        "  PAUSE \r\n" +
+                        ")\r\n\r\n",
+
+                        Path.GetDirectoryName(matcher.Destination) + Path.DirectorySeparator + Path.GetFileName(found),
+                        found
                         ),
 
                         string.Format ("IF EXIST   \"{3}\" (\r\n  ROBOCOPY \"{0}\" \"{1}\" \"{2}\" /MOV\r\n)\r\n\r\n",
@@ -155,35 +207,16 @@ namespace ILFilePacifier
 
                     };
 
-                    File.AppendAllLines (
-                        "Z_" + matcher.What.FirstOrDefault () + "_2F.cmd",
-                        lines,
-                        Encoding.Default);
-                }
+                File.AppendAllLines (
+                    "Z_!ALL_2F.cmd",
+                    lines,
+                    Encoding.Default);
 
-                c = 0;
-                foreach (var found in foundsdd) {
-                    var lines = new String[] {
-                        string.Format ("TITLE PROCESSING D {0} / {1} - \"{2}\"\r\n", ++c, foundsdd.Count, found),
-
-                        string.Format ("IF EXIST   \"{0}\" PAUSE \r\n",
-                        Path.GetDirectoryName(matcher.Destination) + Path.DirectorySeparator + new DirectoryInfo(found).Name
-                        ),
-
-                        string.Format ("IF EXIST   \"{0}\" (\r\n  ROBOCOPY \"{0}\" \"{1}\" /MOV\r\n)\r\n\r\n",
-                        found,
-                        Path.GetDirectoryName(matcher.Destination) + Path.DirectorySeparator + new DirectoryInfo(found).Name
-                        ),
-
-                    };
-
-                    File.AppendAllLines (
-                        "Z_" + matcher.What.FirstOrDefault () + "_1D.cmd",
-                        lines,
-                        Encoding.Default);
-                }
+                File.AppendAllLines (
+                    "Z_" + matcher.What.FirstOrDefault () + "_2F.cmd",
+                    lines,
+                    Encoding.Default);
             }
-
         }
     }
 }
