99deeba84ba8ec91e0978590beb6b22eaefe4265
MTez <mtez1@example.com>
2022-12-20 15:15:04 +0100

    OK

---
+++ ILMagnetToURI/MainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace ILMagnetToURI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.txtURLs = new System.Windows.Forms.TextBox();
+            this.txtHashIds = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.timerClipMonitor = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(9, 3);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(75, 23);
+            this.btnParse.TabIndex = 0;
+            this.btnParse.Text = "Pars&e";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // txtURLs
+            // 
+            this.txtURLs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtURLs.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtURLs.Location = new System.Drawing.Point(3, 3);
+            this.txtURLs.Multiline = true;
+            this.txtURLs.Name = "txtURLs";
+            this.txtURLs.Size = new System.Drawing.Size(836, 355);
+            this.txtURLs.TabIndex = 1;
+            // 
+            // txtHashIds
+            // 
+            this.txtHashIds.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtHashIds.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHashIds.Location = new System.Drawing.Point(845, 3);
+            this.txtHashIds.Multiline = true;
+            this.txtHashIds.Name = "txtHashIds";
+            this.txtHashIds.Size = new System.Drawing.Size(836, 355);
+            this.txtHashIds.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.txtURLs, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtHashIds, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1684, 467);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnOpen);
+            this.panel1.Controls.Add(this.btnParse);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 364);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(836, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(90, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Ope&n";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // timerClipMonitor
+            // 
+            this.timerClipMonitor.Enabled = true;
+            this.timerClipMonitor.Interval = 333;
+            this.timerClipMonitor.Tick += new System.EventHandler(this.timerClipMonitor_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1684, 467);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.TextBox txtURLs;
+        private System.Windows.Forms.TextBox txtHashIds;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Timer timerClipMonitor;
+    }
+}
+
---
+++ ILMagnetToURI/MainForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ILMagnetToURI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm ()
+        {
+            InitializeComponent ();
+        }
+
+        private void btnParse_Click (object sender, EventArgs e)
+        {
+            ParseURLs ();
+        }
+
+        private void btnOpen_Click (object sender, EventArgs e)
+        {
+            OpenInBrowser ();
+        }
+
+
+        private void ParseURLs ()
+        {
+            txtHashIds.Clear ();
+
+            foreach (var url in txtURLs.Lines) {
+                MonoTorrent.MagnetLink u;
+
+                if (MonoTorrent.MagnetLink.TryParse (url, out u)) {
+                    txtHashIds.Text +=
+                        u.InfoHashes.V1.ToHex ()
+                        + Environment.NewLine;
+                }
+
+            }
+        }
+
+        private void OpenInBrowser ()
+        {
+            var baseURLs = new string[] {
+            "https://itorrents.org/torrent/{0}.torrent",
+            "https://torrage.info/torrent.php?h={0}",
+            "https://btcache.me/torrent/{0}"
+            };
+
+            if (string.IsNullOrWhiteSpace (txtHashIds.Text)) {
+                ParseURLs ();
+            }
+
+            if (!string.IsNullOrWhiteSpace (txtHashIds.Text)) {
+                foreach (var hashId in txtHashIds.Lines) {
+                    foreach (var baseURL in baseURLs) {
+                        if (!string.IsNullOrWhiteSpace (hashId)) {
+
+                            Process.Start (
+                                new ProcessStartInfo {
+                                    UseShellExecute = true,
+                                    FileName = string.Format (baseURL, hashId)
+                                });
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private void timerClipMonitor_Tick (object sender, EventArgs e)
+        {
+            var c = Clipboard.GetText ();
+
+            if (string.IsNullOrWhiteSpace (c))
+                return;
+
+            if (c.IndexOf ("magnet:") >= 0) {
+                c = Regex.Replace (c.Trim (), "^\"|\"$", "");
+
+                MonoTorrent.MagnetLink u;
+
+                if (MonoTorrent.MagnetLink.TryParse (c, out u)) {
+
+                    if (!Array.Exists (
+                            txtURLs.Lines,
+                            m => m.IndexOf (u.InfoHashes.V1.ToHex (), StringComparison.InvariantCultureIgnoreCase) >= 0
+                        )) {
+
+                        if (!string.IsNullOrWhiteSpace (txtURLs.Text))
+                            txtURLs.Text += Environment.NewLine;
+
+                        txtURLs.Text += c;
+                    }
+
+                }
+            }
+        }
+    }
+}
